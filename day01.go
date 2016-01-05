fada86435a2f0241990445acca4a5117cdd396c3
JaderDias <jaderd+github@gmail.com>
2016-01-05 21:00:22 +0100

    add solution for day #1

---
+++ day01.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("day01.input")
+	if err != nil {
+		panic(err)
+	}
+
+	floor := 0
+	b1 := make([]byte, 1)
+	for {
+		_, err := f.Read(b1)
+		if err != nil {
+			break
+		}
+
+		if string(b1) == "(" {
+			floor++
+		} else if string(b1) == ")" {
+			floor--
+		}
+	}
+
+	fmt.Println(floor)
+}
